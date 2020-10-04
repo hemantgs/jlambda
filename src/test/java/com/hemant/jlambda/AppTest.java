8eae6017765a8f31897c6223f7ab7d3e627157b0
hemantgs <gshemant@gmail.com>
2020-10-04 12:44:45 +0530

    First tests for -h and -h absent errors

--- src/test/java/com/hemant/jlambda/AppTest.java
+++ src/test/java/com/hemant/jlambda/AppTest.java
@@ -3,8 +3,53 @@
  */
 package com.hemant.jlambda;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+    App app;
+    CommandLine commandLine;
+    StringWriter stringWriter;
+    StringWriter errWriter;
+
+    @BeforeEach
+    public void setup() {
+        app = new App();
+        commandLine = new CommandLine(app);
+        stringWriter = new StringWriter();
+        errWriter = new StringWriter();
+        commandLine.setOut(new PrintWriter(stringWriter));
+        commandLine.setErr(new PrintWriter(errWriter));
+    }
+
+    @Test
+    void shouldDisplayCorrectHelp() {
+        int code = commandLine.execute("-h");
+        assertThat(code).isNotNegative();
+        assertThat(stringWriter.toString()).contains("Usage: jlambda [-h] (-g=<path to generate lambda> | -b=<path to build package\n" +
+                "               to> | [-p=<publish> [-e=<env>]])\n" +
+                "  -b, --build=<path to build package to>\n" +
+                "\n" +
+                "  -e, --env=<env>\n" +
+                "  -g, --generate=<path to generate lambda>\n" +
+                "\n" +
+                "  -h, --help\n" +
+                "  -p, --publish=<publish>");
+    }
+
+    @Test
+    void shouldThrowMissingArgumentErrorWithoutAnyCommand() {
+        int code = commandLine.execute();
+        assertThat(code).isEqualTo(2);
+        assertThat(stringWriter.toString()).isEmpty();
+        assertThat(errWriter.toString()).contains("Error: Missing required argument");
+    }
 }
